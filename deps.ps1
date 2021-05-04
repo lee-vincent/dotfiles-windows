33dfe976eed62c3af8e134c6468eca623eb7776d
lee-vincent <vinnie@vinnielee.io>
2021-05-04 08:48:01 -0400

    remove IIS, node, vim features

--- deps.ps1
+++ deps.ps1
@@ -9,6 +9,7 @@ if (!(Verify-Elevated)) {
 }
 
 
+
 ### Update Help for Modules
 Write-Host "Updating Help..." -ForegroundColor "Yellow"
 Update-Help -Force
@@ -37,78 +38,20 @@ if ((which cinst) -eq $null) {
 }
 
 # system and cli
-choco install curl                --limit-output
-choco install nuget.commandline   --limit-output
-choco install webpi               --limit-output
-choco install git.install         --limit-output -params '"/GitAndUnixToolsOnPath /NoShellIntegration"'
-choco install nvm.portable        --limit-output
-choco install python              --limit-output
-choco install ruby                --limit-output
+choco install curl                  --limit-output
+choco install nuget.commandline     --limit-output
+# choco install webpi               --limit-output
+choco install git.install           --limit-output -params '"/GitAndUnixToolsOnPath /NoShellIntegration"'
+# choco install nvm.portable        --limit-output
+# choco install python              --limit-output
+# choco install ruby                --limit-output
 
 # browsers
-choco install GoogleChrome        --limit-output; <# pin; evergreen #> choco pin add --name GoogleChrome        --limit-output
+choco install GoogleChrome          --limit-output; <# pin; evergreen #> choco pin add --name GoogleChrome        --limit-output
 
 # dev tools and frameworks
-choco install Fiddler             --limit-output
-choco install winmerge            --limit-output
+choco install wireshark             --limit-output
+# choco install Fiddler             --limit-output
+# choco install winmerge            --limit-output
 
 Refresh-Environment
-
-nvm on
-$nodeLtsVersion = choco search nodejs-lts --limit-output | ConvertFrom-String -TemplateContent "{Name:package-name}\|{Version:1.11.1}" | Select -ExpandProperty "Version"
-nvm install $nodeLtsVersion
-nvm use $nodeLtsVersion
-Remove-Variable nodeLtsVersion
-
-gem pristine --all --env-shebang
-
-### Windows Features
-Write-Host "Installing Windows Features..." -ForegroundColor "Yellow"
-# IIS Base Configuration
-Enable-WindowsOptionalFeature -Online -All -FeatureName `
-    "IIS-BasicAuthentication", `
-    "IIS-DefaultDocument", `
-    "IIS-DirectoryBrowsing", `
-    "IIS-HttpCompressionDynamic", `
-    "IIS-HttpCompressionStatic", `
-    "IIS-HttpErrors", `
-    "IIS-HttpLogging", `
-    "IIS-ISAPIExtensions", `
-    "IIS-ISAPIFilter", `
-    "IIS-ManagementConsole", `
-    "IIS-RequestFiltering", `
-    "IIS-StaticContent", `
-    "IIS-WebSockets", `
-    "IIS-WindowsAuthentication" `
-    -NoRestart | Out-Null
-
-# ASP.NET Base Configuration
-Enable-WindowsOptionalFeature -Online -All -FeatureName `
-    "NetFx3", `
-    "NetFx4-AdvSrvs", `
-    "NetFx4Extended-ASPNET45", `
-    "IIS-NetFxExtensibility", `
-    "IIS-NetFxExtensibility45", `
-    "IIS-ASPNET", `
-    "IIS-ASPNET45" `
-    -NoRestart | Out-Null
-
-# Web Platform Installer for remaining Windows features
-webpicmd /Install /AcceptEula /Products:"UrlRewrite2"
-
-### Node Packages
-Write-Host "Installing Node Packages..." -ForegroundColor "Yellow"
-if (which npm) {
-    npm update npm
-    npm install -g gulp
-    npm install -g mocha
-    npm install -g node-inspector
-    npm install -g yo
-}
-
-### Janus for vim
-Write-Host "Installing Janus..." -ForegroundColor "Yellow"
-if ((which curl) -and (which vim) -and (which rake) -and (which bash)) {
-    curl.exe -L https://bit.ly/janus-bootstrap | bash
-}
-
