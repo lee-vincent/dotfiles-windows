ceae501440a950c3fd86dc53004b99c859e5aa85
lee-vincent <vinnie@vinnielee.io>
2021-05-05 14:32:02 -0400

    layout script

--- setup/install.ps1
+++ setup/install.ps1
@@ -2,6 +2,20 @@ $account = "lee-vincent"
 $repo    = "dotfiles-windows"
 $branch  = "master"
 
+
+$customLayout = "$home\AppData\Local\Microsoft\Windows\Shell\LayoutModification.xml"
+Write-Output '<?xml version="1.0"?>' > $customLayout
+Write-Output '<LayoutModificationTemplate xmlns="http://schemas.microsoft.com/Start/2014/LayoutModification" Version="1" xmlns:start="http://schemas.microsoft.com/Start/2014/StartLayout" xmlns:defaultlayout="http://schemas.microsoft.com/Start/2014/FullDefaultLayout">' >> $customLayout
+Write-Output '  <LayoutOptions StartTileGroupCellWidth="6"/>' >> $customLayout
+Write-Output '  <DefaultLayoutOverride>' >> $customLayout
+Write-Output '      <StartLayoutCollection>' >> $customLayout
+Write-Output '          <defaultlayout:StartLayout GroupCellWidth="6"/>' >> $customLayout
+Write-Output '      </StartLayoutCollection>' >> $customLayout
+Write-Output '  </DefaultLayoutOverride>' >> $customLayout
+Write-Output '</LayoutModificationTemplate>' >> $customLayout
+
+Exit
+
 $localRepoDir = Join-Path $HOME "repos"
 if (![System.IO.Directory]::Exists($localRepoDir)) {[System.IO.Directory]::CreateDirectory($localRepoDir)}
 
