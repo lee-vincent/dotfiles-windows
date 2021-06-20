eb942ac587845d8d03ef530d4c3536528c6be057
lee-vincent <vinnie@vinnielee.io>
2021-06-20 12:35:13 -0400

    add notes to bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,7 +1,10 @@
 $profileDir = Split-Path -parent $profile
 $componentDir = Join-Path $profileDir "components"
 
+# create C:\Users\Vinnie\Documents\WindowsPowerShell directory
 New-Item $profileDir -ItemType Directory -Force -ErrorAction SilentlyContinue
+
+# create C:\Users\Vinnie\Documents\WindowsPowerShell\components directory
 New-Item $componentDir -ItemType Directory -Force -ErrorAction SilentlyContinue
 
 Copy-Item -Path ./*.ps1 -Destination $profileDir -Exclude "bootstrap.ps1"
