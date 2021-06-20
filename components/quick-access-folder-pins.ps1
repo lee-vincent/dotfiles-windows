67452a4acb30eeb28c5cd82cd8e9ba7872977b11
lee-vincent <vinnie@vinnielee.io>
2021-06-20 19:53:48 -0400

    create QuickAccessFolderPinner class

---
+++ components/quick-access-folder-pins.ps1
@@ -0,0 +1,32 @@
+Add-Type -TypeDefinition @'
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+
+public class QuickAccessFolderPinner
+{
+    public static void AddFolderToQuickAccess(string pathToFolder)
+    {
+        using (var runspace = RunspaceFactory.CreateRunspace())
+        {
+            runspace.Open();
+            var ps = PowerShell.Create();
+            var shellApplication = ps.AddCommand("New-Object").AddParameter("ComObject", "shell.application").Invoke();
+            dynamic nameSpace = shellApplication.FirstOrDefault()?.Methods["NameSpace"].Invoke(pathToFolder);
+            nameSpace?.Self.InvokeVerb("pintohome");
+        }
+    }
+
+    public static void RemoveFolderFromQuickAccess(string pathToFolder)
+    {
+        using (var runspace = RunspaceFactory.CreateRunspace())
+        {
+            runspace.Open();
+            var ps = PowerShell.Create();
+            var removeScript = "((New-Object -ComObject shell.application).Namespace(\"shell:::{{679f85cb-0220-4080-b29b-5540cc05aab6}}\").Items() | Where-Object {{ $_.Path -EQ \"{pathToFolder}\" }}).InvokeVerb(\"unpinfromhome\")";
+            ps.AddScript(removeScript);
+            ps.Invoke();
+        }
+    }
+}
+'@
