4f86de6d583a4fdbf69f82f2c19d1510be81930e
lee-vincent <vinnie@vinnielee.io>
2021-06-30 11:52:26 -0400

    add Remove-TaskbarPin function

--- functions.ps1
+++ functions.ps1
@@ -335,3 +335,11 @@ function Unzip-File {
         }
     }
 }
+
+function Remove-TaskbarPin([String] $appname)
+{
+    ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() |
+    Where-Object {$_.Name -eq $appname}).Verbs() | 
+    Where-Object {$_.Name.replace('&','') -match 'Unpin from taskbar'} |
+    ForEach-Object {$_.DoIt()}
+}
